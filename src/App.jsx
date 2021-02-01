36d008e7e8780819afac0d395b7e0165886a0c2b
Raghuveer Bharadwaj <raghuveer@billme.co.in>
2021-02-01 23:33:21 +0530

    Finalest changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,8 @@
-import { useEffect } from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import { createBrowserHistory } from "history"
-import ReactGA from "react-ga"
 import { Contact, Home } from './pages'
 import './App.css'
 
 const App = () => {
-  ReactGA.initialize("G-SQGTR7YZT7")
-  const history = createBrowserHistory();
-  history.listen(location => {
-    ReactGA.set({ page: location.pathname })
-    ReactGA.pageview(location.pathname)
-  })
-
-  useEffect(() => {
-    ReactGA.pageview(window.location.pathname)
-  }, [])
   return (
     <Router>
       <Switch>
