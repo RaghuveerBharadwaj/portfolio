107046cda014c64fec124f011f4f3f74493567f1
Raghuveer Bharadwaj <51020296+RaghuveerBharadwaj@users.noreply.github.com>
2024-01-28 00:28:36 +0530

    Updated Description

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -66,13 +66,11 @@ export const Landing = ({ index, isDarkMode }) => {
             I'm a <b>full-stack web developer</b> and a part-time{" "}
             <b>freelancer</b> living in Bengaluru. A mechanical engineer who has
             turned his ardency towards web development and commenced my
-            peregrination as a Software Engineer 4 years ago. Currently, I work
-            as a <b>Senior Frontend Developer</b> at Billme, which makes digital
-            bills for companies such as McDonald's, Burger King, etc. For me,
+            peregrination as a Software Engineer 6 years ago. Currently, I work
+            as a <b>Senior Associate Technology</b> at <b>GALE</b>. For me,
             coding is like solving puzzles. Once you put those jigsaw bits in
             their right places, you have yourself a beauteously functional
-            website. <br />
-            Interested to know more? &nbsp;
+            website. Interested to know more? &nbsp;
             <a
               target="_blank"
               rel="noopener"
